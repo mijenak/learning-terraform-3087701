bd1aabba52cd88dbd4380b9b3b8ea30148f91864
mijenak <62922953+mijenak@users.noreply.github.com>
2024-12-02 22:40:46 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,8 +16,7 @@ data "aws_ami" "app_ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"  
-  subnet_id     = "arn:aws:ec2:ap-southeast-2:027542515554:subnet/subnet-cd4c4784"
+  instance_type = "t3.nano"
 
   tags = {
     Name = "HelloWorld"
