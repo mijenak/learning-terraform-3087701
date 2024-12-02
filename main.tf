2b3f2c60b856d1dcf6571eae7e87c7dbddd9271c
mijenak <62922953+mijenak@users.noreply.github.com>
2024-12-03 03:12:09 +1100

    try previous version

--- main.tf
+++ main.tf
@@ -31,60 +31,52 @@ module "blog_vpc" {
   }
 }
 
-module "blog_asg" {
-  source   = "terraform-aws-modules/autoscaling/aws"
-  version  = "8.0.0"
+module "blog_autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
 
-  name     = "blog_asg"
-  min_size = 1
-  max_size = 1
+  name = "blog"
 
-  vpc_zone_identifier  = module.blog_vpc.public_subnets
-
-  security_groups      = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami
-  instance_type = var.instance_type
-}
-
-resource "aws_autoscaling_attachment" "blog_asg_to_elb" {
-  autoscaling_group_name = module.blog_asg.autoscaling_group_id
-  lb_target_group_arn    = module.blog_alb.arn
+  min_size            = 1
+  max_size            = 2
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  
+  name = "blog-alb"
+
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  listeners = {
-    http = {
-      port            = 80
-      protocol        = "HTTP"
-      forward = {
-        target_group_key = "instance"
-      }
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
     }
-  }
+  ]
 
-  target_groups = {
-    instance = {
-      name_prefix = "blog-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
-    Environment = "Development"
-    Project     = "LI-Learning-Terraform"
+    Environment = "dev"
   }
 }
 
