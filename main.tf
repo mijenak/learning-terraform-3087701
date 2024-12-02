bc947c183998c7bd08ebde151d242f4ad373b040
mijenak <62922953+mijenak@users.noreply.github.com>
2024-12-03 02:10:27 +1100

    ASG

--- main.tf
+++ main.tf
@@ -31,20 +31,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source   = "terraform-aws-modules/autoscaling/aws"
+  version  = "8.0.0"
+
+  name     = "blog"
+  min_size = 1
+  max_size = 1
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-output "instance_ami" {
-  value = aws_instance.blog.ami
-}
-
-output "instance_arn" {
-  value = aws_instance.blog.arn
-}
-
 output "region" {
   description = "Details about selected AWS region"
   value       = data.aws_region.current.name
