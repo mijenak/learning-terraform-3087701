10cdbe85babe0a6af56083d1df5c817878355789
mijenak <62922953+mijenak@users.noreply.github.com>
2024-12-03 04:10:04 +1100

    modularise

--- modules/blog/outputs.tf
+++ modules/blog/outputs.tf
@@ -2,3 +2,7 @@ output "region" {
   description = "Details about selected AWS region"
   value       = data.aws_region.current.name
 }
+
+output "environment_url" {
+  module.blog_alb.lb_dns_name
+}
