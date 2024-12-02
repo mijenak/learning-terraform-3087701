bb61ce0d7675eeead917e13723ccd6bcfcb7d1f7
mijenak <62922953+mijenak@users.noreply.github.com>
2024-12-02 23:52:41 +1100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -4,9 +4,9 @@ output "instance_ami" {
 
 output "instance_arn" {
   value = aws_instance.blog.arn
-
+}
 
 output "region" {
   description = "Details about selected AWS region"
   value       = data.aws_region.current.name
-}
+
